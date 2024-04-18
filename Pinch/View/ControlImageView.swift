4539f46bd79ff9b06b9602ed263c1cdbfe003dec
Amir Vahedi <amirvahedix@gmail.com>
2024-04-18 16:05:20 +0330

    Re-indented files

--- Pinch/View/ControlImageView.swift
+++ Pinch/View/ControlImageView.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct ControlImageView: View {
-    let icon: String
-    
-    var body: some View {
-        Image(systemName: icon)
-            .font(.system(size: 36))
-    }
+  let icon: String
+  
+  var body: some View {
+    Image(systemName: icon)
+      .font(.system(size: 36))
+  }
 }
 
 #Preview (traits: .sizeThatFitsLayout) {
-    ControlImageView(icon: "minus.magnifyingglass")
-        .preferredColorScheme(.dark)
-        .padding()
+  ControlImageView(icon: "minus.magnifyingglass")
+    .preferredColorScheme(.dark)
+    .padding()
 }
