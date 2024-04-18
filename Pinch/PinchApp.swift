b91d4d8ae6142cb24e2320fe214041367d03f73e
Amir Vahedi <amirvahedix@gmail.com>
2024-04-18 13:56:12 +0330

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Amir on 1/30/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
